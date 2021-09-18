3c6be27e101042eecaf763f929a96958573b659a
Shad <shadbabz88@gmail.com>
2021-09-19 02:19:48 +0530

    front image added.skip and watch video after 30 mins

--- src/onlineexaminationsystem_ui/OnlineExaminationSystem_UI.java
+++ src/onlineexaminationsystem_ui/OnlineExaminationSystem_UI.java
@@ -7,7 +7,30 @@
 package onlineexaminationsystem_ui;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
-public class OnlineExaminationSystem_UI extends JFrame implements ActionListener
+
+public class OnlineExaminationSystem_UI extends JFrame
 {
+    OnlineExaminationSystem_UI()
+    {
+        setBounds(500,200,1000,500);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1= new ImageIcon(ClassLoader.getSystemResource("onlineexaminationsystem_ui/icons/welcome_exam.jpg"));
+        JLabel l1=new JLabel(i1);
+        l1.setBounds(0,0,1000,500);
+        add(l1);
+        
+        JLabel l2=new JLabel("Welcome to take your Examination");
+        l2.setFont(new Font("Raleway",Font.BOLD,26));
+        l2.setBounds(750,40,300,100);
+        
+        
+        setVisible(true);
+    }
+    public static void main(String[] args)
+    {
+       new OnlineExaminationSystem_UI(); 
+    }
+}
 
